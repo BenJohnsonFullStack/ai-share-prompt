ced169d315c553320ecf7cd0284ef291a8359b84
Ben Johnson <monk.sound.design@gmail.com>
2023-10-13 18:36:45 -0400

    form submit and cancel buttons create post

--- components/Form.jsx
+++ components/Form.jsx
@@ -37,14 +37,32 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
             </span>
           </span>
 
-          <textarea
-            value={post.prompt}
-            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
-            placeholder="Add tags..."
+          <input
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag: e.target.value })}
+            placeholder="Add tag..."
             required
-            className="form_textarea"
+            className="form_input"
           />
         </label>
+
+        <div className="flex-end mx-3 mb-5 gap-4">
+          <Link href="/" className="text-gray-500 text-sm">
+            Cancel
+          </Link>
+
+          <button
+            type="submit"
+            disabled={submitting}
+            className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white"
+          >
+            {submitting && type === "Create"
+              ? "Creating"
+              : submitting && type === "Edit"
+              ? "Editing"
+              : type}
+          </button>
+        </div>
       </form>
     </section>
   );
