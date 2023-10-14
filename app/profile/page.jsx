b8d2ab76e0c0aeb63d67fd97a7921e03c18652f5
Ben Johnson <monk.sound.design@gmail.com>
2023-10-14 16:04:49 -0400

    delete api call and return filtered posts

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -29,7 +29,25 @@ const MyProfile = () => {
     router.push(`/update-prompt?id=${post._id}`);
   };
 
-  const handleDelete = async (post) => {};
+  const handleDelete = async (post) => {
+    const hasConfirmedDelete = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmedDelete) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = myPosts.filter((p) => p._id !== post._id);
+
+        setMyPosts(filteredPosts);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
 
   return (
     <Profile
@@ -37,7 +55,7 @@ const MyProfile = () => {
       desc="Welcome to your personalized profile page."
       data={myPosts}
       handleEdit={handleEdit}
-      hendleDelete={handleDelete}
+      handleDelete={handleDelete}
     />
   );
 };
