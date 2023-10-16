3540b2aa35786828909add1db8410680fdc0331c
Ben Johnson <monk.sound.design@gmail.com>
2023-10-16 11:45:27 -0400

    added user info policy footer

---
+++ components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <p className="font-inter text-xs text-gray-500 py-6 text-center">
+      *Promptopia collects email addresses, usernames, and content created by
+      users for this app. All information on the web is subject to some risk of
+      exposure, but we have taken steps to ensure the security of your
+      information.
+    </p>
+  );
+};
+
+export default Footer;
